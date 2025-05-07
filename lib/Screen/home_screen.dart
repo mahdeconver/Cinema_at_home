7cb907d6d8bd46cb54f07b0bc6419d3abaabc00d
Yassin Yacoub <97310375+yaseen20001@users.noreply.github.com>
2025-05-07 15:09:58 +0300

    s

--- lib/Screen/home_screen.dart
+++ lib/Screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class HomeScreenNew extends StatelessWidget {
+  const HomeScreenNew({super.key});
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
-      home: const SplashScreen(),
+      home: const SplashScreen(), 
     );
   }
 }
